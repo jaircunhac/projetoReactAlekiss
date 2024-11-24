6ef51646a3b415f4f85e447c685ad91e32d4ddf2
jaircunhac <jaircunhacneto@gmail.com>
2024-11-24 19:46:10 -0300

    feat: pagina Locations e estrutura basica do projeto prontas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Locations from './pages/Locations'
+import { Header } from './components/header/header'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/locations' element={<Locations />} />
+        
+
+      </Routes>
+    </BrowserRouter>
+    
+  </StrictMode>
 )
